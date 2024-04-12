1bd76e7a706e6cb9ff779ff465b8fd48e8fa3a0f
DukeMinty <atorby123@gmail.com>
2024-04-12 15:15:31 -0500

    creation of Shopper class

---
+++ src/main/java/edu/mu/maven/users/Shopper.java
@@ -0,0 +1,36 @@
+package edu.mu.maven.users;
+
+import java.util.ArrayList;
+
+public class Shopper extends Users {
+    private double accountBalance;
+    
+    //Change to Item type when it is made
+    //
+    private ArrayList<String> shoppingCart;
+    //
+    //
+    
+    public Shopper(String username, String password, double accountBalance) {
+    	super(username, password);
+    	this.accountBalance = accountBalance;
+    	shoppingCart = new ArrayList<>();
+    }
+
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+
+	public ArrayList<String> getShoppingCart() {
+		return shoppingCart;
+	}
+
+	public void setShoppingCart(ArrayList<String> shoppingCart) {
+		this.shoppingCart = shoppingCart;
+	}
+    
+}
