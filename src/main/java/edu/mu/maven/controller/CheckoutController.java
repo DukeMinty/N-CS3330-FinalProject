05c4b7dc9a190e4083705589f4619983d51ec80a
Christian Kiner <christiankiner21@icloud.com>
2024-05-06 20:16:32 -0500

    Created the CheckoutController

---
+++ src/main/java/edu/mu/maven/controller/CheckoutController.java
@@ -0,0 +1,59 @@
+package edu.mu.maven.controller;
+
+import java.util.ArrayList;
+
+import edu.mu.maven.address.Address;
+import edu.mu.maven.inventory.Item;
+import edu.mu.maven.model.CheckoutModel;
+import edu.mu.maven.view.CheckoutView;
+
+public class CheckoutController {
+	private CheckoutModel model = null;
+	private CheckoutView view = null;
+	
+	public CheckoutController() {
+		model = new CheckoutModel();
+		view = new CheckoutView();
+	}
+	
+	public void setCart(ArrayList<Item> userCart) {
+		model.setCart(userCart);
+	}
+	
+	public void setAddress(Address shippingAddress) {
+		model.setShippingAddress(shippingAddress);
+	}
+	
+	public void setArrivalDate(String date) {
+		model.setArrivalDate(date);
+	}
+	
+	public double getCartTotal() {
+		double total = 0;
+		for (Item i : model.getCart()) {
+			total += i.getPrice();
+		}
+		
+		return total;
+	}
+	
+	public void printCart() {
+		System.out.println("Your cart currently contains: ");
+		for (Item i : model.getCart()) {
+			System.out.println("\t" + i.getName());
+		}
+	}
+	
+	public void updateView() {
+		view.setArrivalDate(model.getArrivalDate());
+		view.setShippingAddress(model.getShippingAddress());
+		view.setCartTotal(getCartTotal());
+	}
+	
+	public CheckoutView getView() {
+		CheckoutView viewCopy = new CheckoutView(view);
+		return viewCopy;
+	}
+	
+	
+}
